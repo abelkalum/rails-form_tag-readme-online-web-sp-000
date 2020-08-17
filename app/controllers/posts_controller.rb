aca12558f86ab725da0cab139592b9c3cf8c567a
Abel Kalum <chumbabel@gmail.com>
2020-08-17 13:54:25 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,10 @@ class PostsController < ApplicationController
     
   end
   
-  
+  def create
+    Post.create(title: params[:post][:title], description: params[:post][:description])
+    redirect_to posts_path
+  end
   
   def index
     @posts = Post.all
